931bf481bac18beae23332bfa2f4f5fd6466362d
Егор Савельев <mrgorv@yandex.ru>
2021-10-21 09:08:26 +0300

    Input Setup

--- technical_six.cpp
+++ technical_six.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <algorithm>
 #include <cmath>
@@ -7,15 +8,23 @@ using namespace std;
 
 const float eps = 1e-7;
 
+/// 3-dimensional node structure.
 struct Node3d {
     float _x = 0;
     float _y = 0;
     float _z = 0;
 };
 
-struct Line {
-    Node3d _begin;
-    Node3d _end;
+/// Node projection result structure.
+struct ProjectReport {
+    size_t _seg = 1;  /// segment number;
+    float _dist = 0;  /// distance to segment;
+    float _param = 0; /// distance from segment beginning to node projection relative to segment length;
+    Node3d _node;     /// projection coordinate.
+
+    bool operator<(ProjectReport other) {
+        return _dist < other._dist;
+    }
 };
 
 struct Vector3d {
@@ -27,7 +36,7 @@ struct Vector3d {
     }
 
     Vector3d prod(const Vector3d& other) const {
-        Vector3d(_y*other._z - _z*other._y, _z*other._x - _x*other._z, _x*other._y - _y*other._x);
+        return Vector3d(_y*other._z - _z*other._y, _z*other._x - _x*other._z, _x*other._y - _y*other._x);
     }
 
     float dot(const Vector3d& other) const {
@@ -39,15 +48,11 @@ struct Vector3d {
     }
 
     Vector3d operator*(float a) {
-        _x *= a;
-        _y *= a;
-        _z *= a;
+    	return Vector3d(_x*a, _y*a, _z*a);
     }
 
     Vector3d operator/(float a) {
-        _x /= a;
-        _y /= a;
-        _z /= a;
+        return Vector3d(_x/a, _y/a, _z/a);
     }
 
     void operator=(const Vector3d& other) {
@@ -61,23 +66,44 @@ struct Vector3d {
     float _z = 0;
 };
 
+struct Segment {
+    Node3d _begin;
+    Node3d _end;
+
+    ProjectReport FindProjectOn(Node3d point) {
+        ProjectReport result;
+
+        Vector3d forward(_begin, _end);
+        Vector3d backward(_end, _begin);
+        Vector3d from_begin(_begin, point);
+        Vector3d from_end(_end, point);
+
+        if (from_begin.dot(forward) < 0) {
+            result._node = _begin;
+            result._dist = from_begin.norm();
+            result._param = 0;
+        }
+        else if (from_end.dot(backward) < 0) {
+            result._node = _end;
+            result._dist = from_end.norm();
+            result._param = 1;
+        }
+        else {
+            float S = forward.prod(from_begin).norm() / 2;
+            result._dist = S / forward.norm();
+            result._param = (1 - pow(result._dist / (from_begin.norm()), 2)) * from_begin.norm() / forward.norm();
+            Vector3d begin_to_perp = forward * result._param;
+            result._node._x = _begin._x + begin_to_perp._x;
+            result._node._y = _begin._y + begin_to_perp._y;
+            result._node._y = _begin._z + begin_to_perp._z;
+        }
+        return result;
+    }
+
+};
+
 class Polyline {
 public:
-    struct ProjectReport {
-        size_t _seg = 1;
-        float _dist = 0;
-        float _param = 0;
-        Node3d _node;
-
-        bool operator<(ProjectReport other) {
-            return _dist < other._dist;
-        }
-
-        ostream& operator<<(ostream& os) {
-            os << "segment " << _seg << " parameter " << _param << " point " << _node;
-        }
-    };
-
     void AddNode(Node3d node) {
         _nodes.push_back(node);
     }
@@ -89,7 +115,8 @@ public:
     vector<ProjectReport> FindProjects(Node3d point) {
         vector<ProjectReport> result;
         for (size_t i = 0; i < _nodes.size() - 1; ++i) {
-            ProjectReport report = FindClosest(point, {_nodes[i], _nodes[i + 1]});
+        	Segment s = {_nodes[i], _nodes[i + 1]};
+            ProjectReport report = s.FindProjectOn(point);
             report._seg = i + 1;
             result.push_back(report);
         }
@@ -104,43 +131,36 @@ private:
     vector<Node3d> _nodes;
 };
 
-Polyline::ProjectReport FindClosest(Node3d point, Line segment) {
-    Polyline::ProjectReport result;
-
-    Vector3d forward(segment._begin, segment._end);
-    Vector3d backward(segment._end, segment._begin);
-    Vector3d from_begin(segment._begin, point);
-    Vector3d from_end(segment._end, point);
-
-    if (from_begin.dot(forward) < 0) {
-        result._node = segment._begin;
-        result._dist = from_begin.norm();
-        result._param = 0;
-    }
-    else if (from_end.dot(backward) < 0) {
-        result._node = segment._end;
-        result._dist = from_end.norm();
-        result._param = 1;
-    }
-    else {
-        float S = forward.prod(from_begin).norm() / 2;
-        result._dist = S / forward.norm();
-        result._param = (1 - pow(result._dist / (from_begin.norm()), 2)) * from_begin.norm() / forward.norm();
-        Vector3d begin_to_perp = forward * result._param;
-        result._node._x = segment._begin._x + begin_to_perp._x;
-        result._node._y = segment._begin._y + begin_to_perp._y;
-        result._node._y = segment._begin._z + begin_to_perp._z;
-    }
-    return result;
-}
-
 ostream& operator<<(ostream& os, const Node3d& node) {
         os << node._x << ' ' << node._y << ' ' << node._z;
+        return os;
 }
 
-void main() {
-    Node3d point;
+ostream& operator<<(ostream& os, ProjectReport r) {
+        os << "segment " << r._seg << " parameter " << r._param << " point " << r._node;
+        return os;
+}
+
+int main() {
+    Node3d point, temp;
+    Polyline poly;
+    vector<ProjectReport> reports;
+    cout << "Go on" << endl;
+
     string filename;
     cin >> filename;
+    ifstream in(filename);
+    if (in.is_open()) {
+    	while (in >> temp._x >> temp._y >> temp._z) {
+    		poly.AddNode(temp);
+    		cout << temp._x << temp._y << temp._z;
+    	}
+    }
+    in.close();
     cin >> point._x >> point._y >> point._z;
+    reports = poly.FindProjects(point);
+    for (ProjectReport x : reports) {
+    	cout << x << "\n";
+    }
+    return 0;
 }
